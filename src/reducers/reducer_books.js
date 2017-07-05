1a67b7ec2c3042908c7ad25bf218205c11c7dfdd
gabor <gabor@quince.hu>
2017-07-05 20:29:02 +0200

    Added action, reducers and containers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
-export default function() {
+export default function () {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'The Lord of the Rings'}
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 343},
+        {title: 'The Dark Tower', pages: 14},
+        {title: 'The Lord of the Rings', pages: 3534}
     ]
 }
